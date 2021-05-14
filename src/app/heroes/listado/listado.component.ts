26cdbd696367739f9677bc5815f1f62a497f305e
Andres Ocampo <komodo2179@gmail.com>
2021-05-13 22:56:06 -0500

    Primer Commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+  
+})
+export class ListadoComponent {
+
+  heroes: string[] = ['Spider-Man','Iron-Man','Hulk','Thor'];
+  heroeBorrado: string = '';
+
+
+
+  borrarHeroe(){
+    
+    this.heroeBorrado = this.heroes.pop() || '';
+
+  }
+ 
+
+}
